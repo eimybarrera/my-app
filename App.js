83ab18b47de68ff215373d201e8f9e82c4b531d7
eimybarrera <eimybarrera2019@gmail.com>
2024-08-24 08:47:12 -0500

    modificacacion caja H

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.smallBoxes, styles.boxA]}>
+        <Text>A</Text>
+      </View>
+      
+      <View style={styles.boxContainer}>
+        <View style={styles.smallBoxes}>
+          <Text>B</Text>
+        </View>
+        <View style={styles.smallBoxes}>
+          <Text>C</Text>
+        </View>
+        <View style={styles.smallBoxes}>
+          <Text>D</Text>
+        </View>
+      </View>
+
+      <View style={styles.boxContainer}>
+        <View style={styles.smallBoxes}>
+          <Text>E</Text>
+        </View>
+        <View style={styles.smallBoxes}>
+          <Text>F</Text>
+        </View>
+        <View style={styles.smallBoxes}>
+          <Text>G</Text>
+        </View>
+      </View>
+
+      <View style={[styles.smallBoxes, styles.boxH]}>
+        <Text>H</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +43,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
+  boxContainer: {
+    flexDirection: "row",
+  },
+  boxA: {
+    width: 375,
+    height: 190,
+   
+  },
+  smallBoxes: {
+    width: 110,
+    height: 180,
+    backgroundColor: "lightgray",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+  },
+  boxH: {
+    width: 375,
+    height: 250,
+    
+  }
 });
